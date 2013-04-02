c097a5dd7ad3141e34d984e918ca71228f63cf7b
Shansen <me@shansen.me>
2013-04-02 03:54:43 +0200

    Updated GPL headers.

--- src/main/java/me/shansen/EggCatcher/listeners/EggCatcherPlayerListener.java
+++ src/main/java/me/shansen/EggCatcher/listeners/EggCatcherPlayerListener.java
@@ -1,6 +1,6 @@
 /*
 EggCatcher
-Copyright (C) 2012  me@shansen.me
+Copyright (C) 2012, 2013  me@shansen.me
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
